34503155ca4477e759bb030f3445b2f9d57ecad6
Pragathiswaran <mpragathiswaran@gmail.com>
2024-12-31 22:32:32 +0530

    change directory

--- excercise/maths.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        num = input()
-        print(f"{eval(num)}\n")
-    except (ValueError, SyntaxError, NameError) as e:
-        print(f"Invalid input. Please enter a valid input. Error: {e} \n")
